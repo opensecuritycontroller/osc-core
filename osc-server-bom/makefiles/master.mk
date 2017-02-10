c2fcdd288154dc3979132595cf21fcfc6693002a
root <karimullah.mohammed@intel.com>
2017-02-10 09:04:16 -0800

    temporary checking will revert after testing

--- osc-server-bom/makefiles/master.mk
+++ osc-server-bom/makefiles/master.mk
@@ -18,6 +18,16 @@ blddir=$(realpath $(makedir)/..)/build
 bindir=$(realpath $(makedir)/..)/bin
 
 export buildNumber blddir bindir
+export http_proxy=http://proxy-us.intel.com:911
+export https_proxy=http://proxy-us.intel.com:912
+export ftp_proxy=http://proxy-us.intel.com:911
+export socks_proxy=http://proxy-us.intel.com:1080
+export HTTP_PROXY=http://proxy-us.intel.com:911
+export HTTPS_PROXY=http://proxy-us.intel.com:912
+export FTP_PROXY=http://proxy-us.intel.com:911
+export SOCKS_PROXY=http://proxy-us.intel.com:1080
+export NO_PROXY=.corp.nai.org,corp.nai.org,intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
+export no_proxy=.corp.nai.org,corp.nai.org,intel.com,.intel.com,10.0.0.0/8,192.168.0.0/16,localhost,.local,127.0.0.0/8,134.134.0.0/16
 
 dowithbindmount= \
      mount -o loop boot.img $(blddir)/boot && \
