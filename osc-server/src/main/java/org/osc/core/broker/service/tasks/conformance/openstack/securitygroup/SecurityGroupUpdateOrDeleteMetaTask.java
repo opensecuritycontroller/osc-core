523984c869a29943ff1c5e703280f27618da349b
Emanoel Xavier <emanoel.xavier@intel.com>
2017-03-14 19:12:40 -0700

    Fixing bug when creating a security group using a tenant different than the one in the VC

--- osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/openstack/securitygroup/SecurityGroupUpdateOrDeleteMetaTask.java
+++ osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/openstack/securitygroup/SecurityGroupUpdateOrDeleteMetaTask.java
@@ -69,7 +69,7 @@ class SecurityGroupUpdateOrDeleteMetaTask extends TransactionalMetaTask {
 
     @Override
     public void executeTransaction(Session session) throws Exception {
-        this.sg = (SecurityGroup) session.get(SecurityGroup.class, this.sg.getId());
+        this.sg = session.get(SecurityGroup.class, this.sg.getId());
 
         this.tg = new TaskGraph();
 
@@ -132,7 +132,8 @@ class SecurityGroupUpdateOrDeleteMetaTask extends TransactionalMetaTask {
             vdc = new VmDiscoveryCache(this.sg.getVirtualizationConnector(), this.sg.getVirtualizationConnector()
                     .getProviderAdminTenantName());
         } else {
-            vdc = new VmDiscoveryCache(this.sg.getVirtualizationConnector(), this.sg.getTenantName());
+            vdc = new VmDiscoveryCache(this.sg.getVirtualizationConnector(), this.sg.getVirtualizationConnector()
+                    .getProviderAdminTenantName());
         }
 
         // SGM Member sync with no task deferred
