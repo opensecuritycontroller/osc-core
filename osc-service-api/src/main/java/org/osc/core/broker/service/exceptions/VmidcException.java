59e3d565c8d0c77671afa6e56b290c1894efd475
wardtimx <timothyx.ward@intel.com>
2017-04-13 10:20:27 +0100

    [dtos] Break the one remaining dependency from the Exceptions to the core server

--- osc-service-api/src/main/java/org/osc/core/broker/service/exceptions/VmidcException.java
+++ osc-service-api/src/main/java/org/osc/core/broker/service/exceptions/VmidcException.java
@@ -16,13 +16,11 @@
  *******************************************************************************/
 package org.osc.core.broker.service.exceptions;
 
-import com.mcafee.vmidc.server.Server;
-
 public class VmidcException extends Exception {
     private static final long serialVersionUID = 1L;
 
     public VmidcException(String s) {
-        super(Server.PRODUCT_NAME + ": " + s);
+        super("Open Security Controller: " + s);
     }
 
     public VmidcException(Throwable e) {
