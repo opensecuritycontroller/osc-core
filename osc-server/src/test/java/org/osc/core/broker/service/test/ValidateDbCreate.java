ab197a4a21da316984f0dab8023b5e818ceac253
karimull <karimullah.mohammed@intel.com>
2017-05-05 01:51:55 -0700

    DE3945: Remove McAfee reference in osc files (#228)

--- osc-server/src/test/java/org/osc/core/broker/service/test/ValidateDbCreate.java
+++ osc-server/src/test/java/org/osc/core/broker/service/test/ValidateDbCreate.java
@@ -246,7 +246,7 @@ public class ValidateDbCreate {
         user.setLastName("Lee");
         user.setLoginName("tlee");
         user.setRole(RoleType.ADMIN);
-        user.setEmail("trinh_lee@mcafee.com");
+        user.setEmail("trinh_lee@dummy.com");
 
         OSCEntityManager.create(em, user, this.txBroadcastUtil);
 
