5009744d0743d4b6d85fc22a4a46274586c12087
Emanoel Xavier <emanoel.xavier@intel.com>
2017-06-01 17:09:57 -0700

    Adding unit tests for Create/UpdatePortGroupHookTask

---
+++ osc-server/src/test/java/org/osc/core/broker/service/tasks/conformance/openstack/securitygroup/BasePortGroupHookTaskTest.java
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ * Copyright (c) Intel Corporation
+ * Copyright (c) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.osc.core.broker.service.tasks.conformance.openstack.securitygroup;
+
+import static org.mockito.Mockito.when;
+
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.osc.core.broker.model.entities.appliance.DistributedApplianceInstance;
+import org.osc.core.broker.model.entities.appliance.TagEncapsulationType;
+import org.osc.core.broker.model.entities.appliance.VirtualSystem;
+import org.osc.core.broker.model.entities.virtualization.FailurePolicyType;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroup;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroupInterface;
+import org.osc.core.broker.model.plugin.sdncontroller.SdnControllerApiFactory;
+import org.osc.core.broker.service.exceptions.VmidcBrokerValidationException;
+import org.osc.core.broker.util.db.HibernateUtil;
+import org.osc.core.test.util.TestTransactionControl;
+import org.osc.sdk.controller.api.SdnRedirectionApi;
+import org.osc.sdk.controller.element.InspectionPortElement;
+import org.powermock.api.mockito.PowerMockito;
+
+public class BasePortGroupHookTaskTest {
+    @Mock protected EntityManager em;
+    @Mock protected EntityTransaction tx;
+
+    @Mock(answer=Answers.CALLS_REAL_METHODS)
+    TestTransactionControl txControl;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void testInitialize() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        when(this.em.getTransaction()).thenReturn(this.tx);
+
+        this.txControl.setEntityManager(this.em);
+
+        PowerMockito.mockStatic(HibernateUtil.class);
+        when(HibernateUtil.getTransactionalEntityManager()).thenReturn(this.em);
+        when(HibernateUtil.getTransactionControl()).thenReturn(this.txControl);
+    }
+
+    protected void testExecute_WhenSGHasNoNetworkElementId_ThrowsValidationException(boolean create) throws Exception {
+        // Arrange.
+        SecurityGroup sg = newSecurityGroup();
+        sg.setNetworkElementId(null);
+        SecurityGroupInterface sgi = registerNewSGI(sg, 1L);
+        DistributedApplianceInstance dai = registerNewDAI();
+
+        this.exception.expect(VmidcBrokerValidationException.class);
+        this.exception.expectMessage(String.format("The security group %s does not have a network element set.", sg.getName()));
+
+        BasePortGroupHookTask task = create ? new CreatePortGroupHookTask(sgi, dai) : new UpdatePortGroupHookTask(sgi, dai);
+
+        // Act.
+        task.execute();
+    }
+
+    protected void testExecute_WhenCreateNetworkRedirectionApiFails_ThrowsTheUnhandledException(boolean create) throws Exception {
+        // Arrange.
+        SecurityGroupInterface sgi = registerNewSGI(newSecurityGroup(), 2L);
+        DistributedApplianceInstance dai = registerNewDAI();
+
+        PowerMockito.spy(SdnControllerApiFactory.class);
+        PowerMockito.doThrow(new IllegalStateException()).when(SdnControllerApiFactory.class, "createNetworkRedirectionApi", sgi.getVirtualSystem());
+
+        this.exception.expect(IllegalStateException.class);
+
+        BasePortGroupHookTask task = create ? new CreatePortGroupHookTask(sgi, dai) : new UpdatePortGroupHookTask(sgi, dai);
+
+        // Act.
+        task.execute();
+    }
+
+    protected void registerNetworkRedirectionApi(SdnRedirectionApi redirectionApi, VirtualSystem vs) throws Exception {
+        PowerMockito.spy(SdnControllerApiFactory.class);
+        PowerMockito.doReturn(redirectionApi).when(SdnControllerApiFactory.class, "createNetworkRedirectionApi", vs);
+
+    }
+
+    protected SecurityGroup newSecurityGroup() {
+        SecurityGroup sg = new SecurityGroup(null, null, null);
+        sg.setNetworkElementId(UUID.randomUUID().toString());
+        sg.setName(UUID.randomUUID().toString());
+        return sg;
+    }
+
+    protected SecurityGroupInterface registerNewSGI(SecurityGroup sg, Long entityId) {
+        VirtualSystem vs = new VirtualSystem(null);
+        vs.setId(entityId);
+        vs.setEncapsulationType(TagEncapsulationType.VLAN);
+
+        SecurityGroupInterface sgi = new SecurityGroupInterface(vs, null, entityId.toString(), FailurePolicyType.FAIL_CLOSE, 1L);
+        sgi.setId(entityId);
+
+        if (sg != null) {
+            sgi.addSecurityGroup(sg);
+        }
+
+        when(this.em.find(SecurityGroupInterface.class, sgi.getId())).thenReturn(sgi);
+        return sgi;
+    }
+
+    protected DistributedApplianceInstance registerNewDAI() {
+        DistributedApplianceInstance dai = new DistributedApplianceInstance();
+        dai.setName("dai-name");
+        dai.setId(1L);
+        dai.setInspectionEgressMacAddress(UUID.randomUUID().toString());
+        dai.setInspectionOsEgressPortId(UUID.randomUUID().toString());
+        dai.setInspectionIngressMacAddress(UUID.randomUUID().toString());
+        dai.setInspectionOsIngressPortId(UUID.randomUUID().toString());
+
+        when(this.em.find(DistributedApplianceInstance.class, dai.getId())).thenReturn(dai);
+        return dai;
+    }
+
+    protected boolean inspectionPortMatchesDAI(InspectionPortElement inspectionPort, DistributedApplianceInstance dai) {
+        return inspectionPort.getIngressPort().getMacAddresses().size() == 1 &&
+                inspectionPort.getIngressPort().getMacAddresses().get(0).equals(dai.getInspectionIngressMacAddress()) &&
+                inspectionPort.getIngressPort().getElementId().equals(dai.getInspectionOsIngressPortId()) &&
+                inspectionPort.getEgressPort().getMacAddresses().size() == 1 &&
+                inspectionPort.getEgressPort().getMacAddresses().get(0).equals(dai.getInspectionEgressMacAddress()) &&
+                inspectionPort.getEgressPort().getElementId().equals(dai.getInspectionOsEgressPortId());
+    }
+}
---
+++ osc-server/src/test/java/org/osc/core/broker/service/tasks/conformance/openstack/securitygroup/CreatePortGroupHookTaskTest.java
@@ -0,0 +1,182 @@
+/*******************************************************************************
+ * Copyright (c) Intel Corporation
+ * Copyright (c) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.osc.core.broker.service.tasks.conformance.openstack.securitygroup;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.util.UUID;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
+import org.osc.core.broker.model.entities.appliance.DistributedApplianceInstance;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroup;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroupInterface;
+import org.osc.core.broker.model.plugin.sdncontroller.SdnControllerApiFactory;
+import org.osc.core.broker.service.exceptions.VmidcException;
+import org.osc.core.broker.util.db.HibernateUtil;
+import org.osc.sdk.controller.FailurePolicyType;
+import org.osc.sdk.controller.TagEncapsulationType;
+import org.osc.sdk.controller.api.SdnRedirectionApi;
+import org.osc.sdk.controller.element.InspectionPortElement;
+import org.osc.sdk.controller.element.NetworkElement;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ HibernateUtil.class, SdnControllerApiFactory.class })
+public class CreatePortGroupHookTaskTest extends BasePortGroupHookTaskTest {
+    @Test
+    public void testExecute_WhenSGHasNoNetworkElementId_ThrowsValidationException() throws Exception {
+        super.testExecute_WhenSGHasNoNetworkElementId_ThrowsValidationException(true);
+    }
+
+    @Test
+    public void testExecute_WhenCreateNetworkRedirectionApiFails_ThrowsTheUnhandledException() throws Exception {
+        super.testExecute_WhenCreateNetworkRedirectionApiFails_ThrowsTheUnhandledException(true);
+    }
+
+    @Test
+    public void testExecute_WhenInstallInspectionHookFails_ThrowsTheUnhandledException() throws Exception {
+        // Arrange.
+        SecurityGroup sg = newSecurityGroup();
+        SecurityGroupInterface sgi = registerNewSGI(sg, 2L);
+        DistributedApplianceInstance dai = registerNewDAI();
+
+        SdnRedirectionApi redirectionApi = mockInstallInspectionHook(sgi, dai, new IllegalStateException());
+
+        registerNetworkRedirectionApi(redirectionApi, sgi.getVirtualSystem());
+
+        this.exception.expect(IllegalStateException.class);
+
+        CreatePortGroupHookTask task = new CreatePortGroupHookTask(sgi, dai);
+
+        // Act.
+        task.execute();
+    }
+
+    @Test
+    public void testExecute_WhenInstallInspectionHookReturnsNullId_ThrowsVmidcException() throws Exception {
+        // Arrange.
+        SecurityGroup sg = newSecurityGroup();
+        SecurityGroupInterface sgi = registerNewSGI(sg, 3L);
+        DistributedApplianceInstance dai = registerNewDAI();
+
+        SdnRedirectionApi redirectionApi = mockInstallInspectionHook(sgi, dai, (String) null);
+
+        registerNetworkRedirectionApi(redirectionApi, sgi.getVirtualSystem());
+
+        CreatePortGroupHookTask task = new CreatePortGroupHookTask(sgi, dai);
+
+        this.exception.expect(VmidcException.class);
+        this.exception.expectMessage(String.format("The creation of the inspection hook for the security group interface %s."
+                + "succeeded but the returned identifier was null.", sgi.getName()));
+
+        // Act.
+        task.execute();
+    }
+
+    @Test
+    public void testExecute_WhenInstallInspectionHookSucceeds_SGIIsUpdatedWithHookId() throws Exception {
+        // Arrange.
+        SecurityGroup sg = newSecurityGroup();
+        SecurityGroupInterface sgi = registerNewSGI(sg, 4L);
+        DistributedApplianceInstance dai = registerNewDAI();
+
+        String inspectionHookId = UUID.randomUUID().toString();
+
+        SdnRedirectionApi redirectionApi = mockInstallInspectionHook(sgi, dai, inspectionHookId);
+
+        registerNetworkRedirectionApi(redirectionApi, sgi.getVirtualSystem());
+
+        CreatePortGroupHookTask task = new CreatePortGroupHookTask(sgi, dai);
+
+        // Act.
+        task.execute();
+
+        // Assert.
+        Assert.assertEquals("", inspectionHookId, sgi.getNetworkElementId());
+        verify(this.em, Mockito.timeout(1)).merge(sgi);
+    }
+
+    private SdnRedirectionApi mockInstallInspectionHook(SecurityGroupInterface sgi, DistributedApplianceInstance dai, Exception e) throws Exception {
+        SdnRedirectionApi redirectionApi = mock(SdnRedirectionApi.class);
+        when(redirectionApi.installInspectionHook(
+                argThat(new NetworkElementMatcher(sgi.getSecurityGroup())),
+                argThat(new InspectionPortElementMatcher(dai)),
+                eq(Long.parseLong(sgi.getTag())),
+                (TagEncapsulationType)isNull(),
+                eq(sgi.getOrder()),
+                (FailurePolicyType)isNull())).thenThrow(e);
+
+        return redirectionApi;
+    }
+
+    private SdnRedirectionApi mockInstallInspectionHook(SecurityGroupInterface sgi, DistributedApplianceInstance dai, String inspectionHookId) throws Exception {
+        SdnRedirectionApi redirectionApi = mock(SdnRedirectionApi.class);
+        when(redirectionApi.installInspectionHook(
+                argThat(new NetworkElementMatcher(sgi.getSecurityGroup())),
+                argThat(new InspectionPortElementMatcher(dai)),
+                eq(Long.parseLong(sgi.getTag())),
+                (TagEncapsulationType)isNull(),
+                eq(sgi.getOrder()),
+                (FailurePolicyType)isNull())).thenReturn(inspectionHookId);
+
+        return redirectionApi;
+    }
+
+    private class InspectionPortElementMatcher extends ArgumentMatcher<InspectionPortElement> {
+        private DistributedApplianceInstance dai;
+
+        public InspectionPortElementMatcher(DistributedApplianceInstance dai) {
+            this.dai = dai;
+        }
+
+        @Override
+        public boolean matches(Object object) {
+            if (object == null || !(object instanceof InspectionPortElement)) {
+                return false;
+            }
+
+            InspectionPortElement inspectionPort = (InspectionPortElement) object;
+
+            return inspectionPortMatchesDAI(inspectionPort, this.dai);
+        }
+    }
+
+    private class NetworkElementMatcher extends ArgumentMatcher<NetworkElement> {
+        private SecurityGroup sg;
+
+        public NetworkElementMatcher(SecurityGroup sg) {
+            this.sg = sg;
+        }
+
+        @Override
+        public boolean matches(Object object) {
+            if (object == null || !(object instanceof NetworkElement)) {
+                return false;
+            }
+
+            NetworkElement netElement = (NetworkElement) object;
+
+            return netElement.getElementId().equals(this.sg.getNetworkElementId());
+        }
+    }
+}
