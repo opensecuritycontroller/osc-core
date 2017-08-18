dc4a07a4f5d3628593d4a861f81cbaad7ae2456d
dmitry <dmitry.mx.gerenrot@intel.com>
2017-08-18 15:24:27 -0700

    Added UpdateOrDeleteK8sSecurityGroupMetaTask
    * Added UpdateOrDeleteK8sSecurityGroupMetaTask + unit tests
    
    * Fixed SecurityGroupCheckMetaTask to launch the said metatask
    
    * Added stub CheckK8sSecurityGroupLabelMetaTask

---
+++ osc-server/src/test/java/org/osc/core/broker/service/tasks/conformance/openstack/securitygroup/UpdateOrDeleteK8sSecurityGroupMetaTaskTestData.java
@@ -0,0 +1,156 @@
+/*******************************************************************************
+ * Copyright (c) Intel Corporation
+ * Copyright (c) 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.osc.core.broker.service.tasks.conformance.openstack.securitygroup;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+
+import org.osc.core.broker.job.TaskGraph;
+import org.osc.core.broker.model.entities.appliance.Appliance;
+import org.osc.core.broker.model.entities.appliance.ApplianceSoftwareVersion;
+import org.osc.core.broker.model.entities.appliance.DistributedAppliance;
+import org.osc.core.broker.model.entities.appliance.VirtualSystem;
+import org.osc.core.broker.model.entities.management.ApplianceManagerConnector;
+import org.osc.core.broker.model.entities.management.Domain;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroup;
+import org.osc.core.broker.model.entities.virtualization.SecurityGroupMember;
+import org.osc.core.broker.model.entities.virtualization.VirtualizationConnector;
+import org.osc.core.broker.model.entities.virtualization.k8s.Label;
+import org.osc.core.common.virtualization.VirtualizationType;
+
+public class UpdateOrDeleteK8sSecurityGroupMetaTaskTestData {
+
+    public final static String MGR_TYPE = "SMC";
+    private final static int MANY = 4;
+
+    public static SecurityGroup NO_LABEL_SG = createSecurityGroup("NO_LABEL", 0, false);
+    public static SecurityGroup SINGLE_LABEL_SG = createSecurityGroup("SINGLE_LABEL", 1, false);
+    public static SecurityGroup MULTI_LABEL_SG = createSecurityGroup("MULTI_LABEL", MANY, false);
+    public static SecurityGroup SINGLE_LABEL_MARKED_FOR_DELETION_SG = createSecurityGroup("SINGLE_LABEL", 1, true);
+
+    public static TaskGraph createK8sGraph(SecurityGroup sg, boolean isDelete) {
+        TaskGraph expectedGraph = new TaskGraph();
+
+        for (SecurityGroupMember sgm : sg.getSecurityGroupMembers()) {
+            expectedGraph.addTask(new CheckK8sSecurityGroupLabelMetaTask().create(sgm, isDelete));
+        }
+
+        expectedGraph.appendTask(new PortGroupCheckMetaTask().create(sg, isDelete, null));
+
+        return expectedGraph;
+    }
+
+    public static void persistObjects(EntityManager em) {
+        List<SecurityGroup> securityGroups = Arrays.asList(NO_LABEL_SG, SINGLE_LABEL_SG, MULTI_LABEL_SG);
+
+        for (SecurityGroup sg : securityGroups) {
+            if (sg.getId() != null) {
+                continue;
+            }
+
+            Set<VirtualSystem> virtualSystems = sg.getVirtualizationConnector().getVirtualSystems();
+
+            em.getTransaction().begin();
+
+            em.persist(sg.getVirtualizationConnector());
+
+            for (VirtualSystem vs : virtualSystems) {
+                em.persist(vs.getDomain().getApplianceManagerConnector());
+                em.persist(vs.getApplianceSoftwareVersion().getAppliance());
+                em.persist(vs.getApplianceSoftwareVersion());
+                em.persist(vs.getDistributedAppliance());
+                em.persist(vs.getDomain());
+                em.persist(vs);
+            }
+
+            em.persist(sg);
+            for (SecurityGroupMember sgm : sg.getSecurityGroupMembers()) {
+                em.persist(sgm.getLabel());
+                em.persist(sgm);
+            }
+
+            sg.getSecurityGroupMembers();
+            em.getTransaction().commit();
+        }
+    }
+
+    private static SecurityGroup createSecurityGroup(String baseName, int nLabels, boolean isDelete) {
+        VirtualSystem vs = createVirtualSystem(baseName, MGR_TYPE);
+
+        SecurityGroup sg = new SecurityGroup(vs.getVirtualizationConnector(), null, null);
+        sg.setName(baseName + "_sg");
+        sg.setMarkedForDeletion(isDelete);
+
+        for (int i = 0; i < nLabels; i++) {
+            Label label = new Label("LABEL_NAME" + i + sg.getName(), "LABEL_VALUE" + i + sg.getName());
+            SecurityGroupMember sgm = new SecurityGroupMember(sg, label);
+            sg.addSecurityGroupMember(sgm);
+        }
+
+        return sg;
+    }
+
+    private static VirtualSystem createVirtualSystem(String baseName, String mgrType) {
+        VirtualizationConnector vc = new VirtualizationConnector();
+        vc.setName(baseName + "_vc");
+        vc.setVirtualizationType(VirtualizationType.KUBERNETES);
+        vc.setVirtualizationSoftwareVersion("vcSoftwareVersion");
+        vc.setProviderIpAddress(baseName + "_providerIp");
+        vc.setProviderUsername("Natasha");
+        vc.setProviderPassword("********");
+
+        ApplianceManagerConnector mc = new ApplianceManagerConnector();
+        mc.setIpAddress(baseName + "_mcIp");
+        mc.setName(baseName + "_mc");
+        mc.setServiceType("foobar");
+        mc.setManagerType(mgrType.toString());
+
+        Domain domain = new Domain(mc);
+        domain.setName(baseName + "_domain");
+        vc.setAdminDomainId(domain.getName());
+
+        Appliance app = new Appliance();
+        app.setManagerSoftwareVersion("fizz");
+        app.setManagerType(mgrType);
+        app.setModel(baseName + "_model");
+
+        ApplianceSoftwareVersion asv = new ApplianceSoftwareVersion(app);
+        asv.setApplianceSoftwareVersion("softwareVersion");
+        asv.setImageUrl(baseName + "_image");
+        asv.setVirtualizarionSoftwareVersion(vc.getVirtualizationSoftwareVersion());
+        asv.setVirtualizationType(vc.getVirtualizationType());
+
+        DistributedAppliance da = new DistributedAppliance(mc);
+        da.setName(baseName + "_da");
+        da.setApplianceVersion("foo");
+        da.setAppliance(app);
+
+        VirtualSystem vs = new VirtualSystem(da);
+        vs.setApplianceSoftwareVersion(asv);
+        vs.setDomain(domain);
+        vs.setVirtualizationConnector(vc);
+        vs.setMarkedForDeletion(false);
+        vs.setName(baseName + "_vs");
+        vs.setMgrId(baseName + "_mgrId");
+
+        vc.getVirtualSystems().add(vs);
+        return vs;
+    }
+}
