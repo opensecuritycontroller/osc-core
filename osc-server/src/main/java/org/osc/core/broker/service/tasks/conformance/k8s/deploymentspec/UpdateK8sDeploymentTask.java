f3faba2cf27676cc5a96e68943e0d47b59447088
Emanoel Xavier <emanoel.xavier@intel.com>
2017-09-19 07:56:51 -0700

    Adding update k8s deployment task

--- osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/k8s/deploymentspec/UpdateK8sDeploymentTask.java
+++ osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/k8s/deploymentspec/UpdateK8sDeploymentTask.java
@@ -23,6 +23,9 @@ import javax.persistence.EntityManager;
 import org.apache.log4j.Logger;
 import org.osc.core.broker.job.lock.LockObjectReference;
 import org.osc.core.broker.model.entities.virtualization.openstack.DeploymentSpec;
+import org.osc.core.broker.rest.client.k8s.KubernetesClient;
+import org.osc.core.broker.rest.client.k8s.KubernetesDeploymentApi;
+import org.osc.core.broker.service.persistence.OSCEntityManager;
 import org.osc.core.broker.service.tasks.TransactionalTask;
 import org.osgi.service.component.annotations.Component;
 
@@ -32,25 +35,51 @@ public class UpdateK8sDeploymentTask extends TransactionalTask {
 
     private DeploymentSpec ds;
 
-    public UpdateK8sDeploymentTask create(DeploymentSpec ds) {
+    private KubernetesDeploymentApi k8sDeploymentApi;
+
+    UpdateK8sDeploymentTask create(DeploymentSpec ds, KubernetesDeploymentApi k8sDeploymentApi) {
         UpdateK8sDeploymentTask task = new UpdateK8sDeploymentTask();
+        task.dbConnectionManager = this.dbConnectionManager;
+        task.txBroadcastUtil = this.txBroadcastUtil;
         task.ds = ds;
+        task.k8sDeploymentApi = k8sDeploymentApi;
 
         return task;
     }
 
+    public UpdateK8sDeploymentTask create(DeploymentSpec ds) {
+        return create(ds, null);
+    }
+
     @Override
     public void executeTransaction(EntityManager em) throws Exception {
+        OSCEntityManager<DeploymentSpec> dsEmgr = new OSCEntityManager<DeploymentSpec>(DeploymentSpec.class, em, this.txBroadcastUtil);
+        this.ds = dsEmgr.findByPrimaryKey(this.ds.getId());
+
+        try (KubernetesClient client = new KubernetesClient(this.ds.getVirtualSystem().getVirtualizationConnector())) {
+            if (this.k8sDeploymentApi == null) {
+                this.k8sDeploymentApi = new KubernetesDeploymentApi(client);
+            } else {
+                this.k8sDeploymentApi.setKubernetesClient(client);
+            }
+
+            this.k8sDeploymentApi.updateDeploymentReplicaCount(
+                    this.ds.getExternalId(),
+                    this.ds.getNamespace(),
+                    CreateK8sDeploymentTask.getK8sName(this.ds),
+                    this.ds.getInstanceCount());
+
+            LOG.info(String.format("Updated the deployment in kubernetes with the K8s id %s.", this.ds.getExternalId()));
+        }
     }
 
     @Override
     public String getName() {
-        return String.format("Updating the K8s Deployment Spec '%s'", this.ds.getName());
+        return String.format("Updated the K8s deployment spec %s", this.ds.getName());
     }
 
     @Override
     public Set<LockObjectReference> getObjects() {
         return LockObjectReference.getObjectReferences(this.ds);
     }
-
 }
