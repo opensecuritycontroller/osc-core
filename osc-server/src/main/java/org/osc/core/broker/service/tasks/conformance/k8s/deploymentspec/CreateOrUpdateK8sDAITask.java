376a241458f85a457d0e63e4c5a59525968b0a73
Emanoel Xavier <emanoel.xavier@intel.com>
2017-09-20 09:13:46 -0700

    Adding the task to create the k8s dai

--- osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/k8s/deploymentspec/CreateOrUpdateK8sDAITask.java
+++ osc-server/src/main/java/org/osc/core/broker/service/tasks/conformance/k8s/deploymentspec/CreateOrUpdateK8sDAITask.java
@@ -16,18 +16,25 @@
  *******************************************************************************/
 package org.osc.core.broker.service.tasks.conformance.k8s.deploymentspec;
 
+import java.util.Optional;
 import java.util.Set;
 
 import javax.persistence.EntityManager;
 
 import org.apache.log4j.Logger;
 import org.osc.core.broker.job.lock.LockObjectReference;
+import org.osc.core.broker.model.entities.appliance.DistributedApplianceInstance;
 import org.osc.core.broker.model.entities.virtualization.openstack.DeploymentSpec;
+import org.osc.core.broker.model.plugin.ApiFactoryService;
 import org.osc.core.broker.rest.client.k8s.KubernetesDeploymentApi;
 import org.osc.core.broker.rest.client.k8s.KubernetesPod;
+import org.osc.core.broker.service.exceptions.VmidcException;
 import org.osc.core.broker.service.persistence.OSCEntityManager;
 import org.osc.core.broker.service.tasks.TransactionalTask;
+import org.osc.sdk.controller.api.SdnRedirectionApi;
+import org.osc.sdk.controller.element.NetworkElement;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * This task is responsible for persisting a DAI in the OSC database for a newly found pod VNF.
@@ -41,7 +48,8 @@ public class CreateOrUpdateK8sDAITask extends TransactionalTask {
 
     private KubernetesPod k8sPod;
 
-    private KubernetesDeploymentApi k8sDeploymentApi;
+    @Reference
+    private ApiFactoryService apiFactoryService;
 
     public CreateOrUpdateK8sDAITask create(DeploymentSpec ds, KubernetesPod k8sPod) {
         return create(ds, k8sPod, null);
@@ -51,9 +59,9 @@ public class CreateOrUpdateK8sDAITask extends TransactionalTask {
         CreateOrUpdateK8sDAITask task = new CreateOrUpdateK8sDAITask();
         task.dbConnectionManager = this.dbConnectionManager;
         task.txBroadcastUtil = this.txBroadcastUtil;
+        task.apiFactoryService = this.apiFactoryService;
         task.ds = ds;
         task.k8sPod = k8sPod;
-        task.k8sDeploymentApi = k8sDeploymentApi;
 
         return task;
     }
@@ -62,11 +70,55 @@ public class CreateOrUpdateK8sDAITask extends TransactionalTask {
     public void executeTransaction(EntityManager em) throws Exception {
         OSCEntityManager<DeploymentSpec> dsEmgr = new OSCEntityManager<DeploymentSpec>(DeploymentSpec.class, em, this.txBroadcastUtil);
         this.ds = dsEmgr.findByPrimaryKey(this.ds.getId());
+
+        NetworkElement portElement;
+
+        try (SdnRedirectionApi redirection = this.apiFactoryService.createNetworkRedirectionApi(this.ds.getVirtualSystem())) {
+            // TODO emanoel: Replace this id workaround with the pod UID once Nuage supports the look up by PodUID.
+            String deviceOwnerId = this.k8sPod.getNamespace() + ":" + this.k8sPod.getName();
+            portElement = redirection.getNetworkElementByDeviceOwnerId(deviceOwnerId);
+            if (portElement == null) {
+                throw new VmidcException(String.format("The SDN controller did not return a network element for the device id %s" , deviceOwnerId));
+            }
+        }
+
+        // Trying to retrieve a dai that is assigned to an inspection element on the SDN
+        // but does not have an associated port (previously deleted pod)
+        Optional<DistributedApplianceInstance> orphanDai =
+                this.ds.getDistributedApplianceInstances()
+                .stream()
+                .filter(dai -> (dai.getInspectionElementId() != null && dai.getInspectionOsIngressPortId() == null)).findFirst();
+
+        if (orphanDai.isPresent()) {
+            LOG.info(String.format("Found orphan dai with id %s and name %s and inspection element %s",
+                    orphanDai.get().getId(),
+                    orphanDai.get().getName(),
+                    orphanDai.get().getInspectionElementId()));
+        }
+
+        DistributedApplianceInstance dai = new DistributedApplianceInstance(this.ds.getVirtualSystem());
+        dai.setOsHostName(this.k8sPod.getNode());
+        dai.setDeploymentSpec(this.ds);
+        dai.setName(this.k8sPod.getNamespace() + "-" + this.k8sPod.getName());
+        dai.setExternalId(this.k8sPod.getUid());
+        // Creating the new DAI with the existing/orphan DAI inspection element id
+        dai.setInspectionElementId(orphanDai.isPresent() ? orphanDai.get().getInspectionElementId() : null);
+        dai.setInspectionElementParentId(orphanDai.isPresent() ? orphanDai.get().getInspectionElementParentId() : null);
+        dai.setInspectionOsIngressPortId(portElement.getElementId());
+        dai.setInspectionIngressMacAddress(portElement.getMacAddresses().get(0));
+        dai.setIpAddress(portElement.getPortIPs().get(0));
+        dai = OSCEntityManager.create(em, dai, this.txBroadcastUtil);
+        LOG.info(String.format("Created dai %s.", dai.getName()));
+
+        if (orphanDai.isPresent()) {
+            OSCEntityManager.delete(em, orphanDai.get(), this.txBroadcastUtil);
+            LOG.info(String.format("Deleted orphan dai with id %s and name %s", orphanDai.get().getId(), orphanDai.get().getName()));
+        }
     }
 
     @Override
     public String getName() {
-        return String.format("Creating the K8s deployment spec %s and pod %s", this.ds.getName(), this.k8sPod.getUid());
+        return String.format("Creating the K8s dai for deployment spec %s and pod %s", this.ds.getName(), this.k8sPod.getUid());
     }
 
     @Override
