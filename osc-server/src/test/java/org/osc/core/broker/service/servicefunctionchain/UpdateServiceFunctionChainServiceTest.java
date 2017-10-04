495abcbeecdae300ba6c9b3648b507a20d4ec897
karimull <karimullah.mohammed@intel.com>
2017-10-04 08:17:56 -0700

    Added update service function chain unit test APIs to check virtual … (#507)
    *  Added update service function chain unit test APIs to check virtual system order
    
    *  Consolidated to two updated SFC unit tests and change of names

--- osc-server/src/test/java/org/osc/core/broker/service/servicefunctionchain/UpdateServiceFunctionChainServiceTest.java
+++ osc-server/src/test/java/org/osc/core/broker/service/servicefunctionchain/UpdateServiceFunctionChainServiceTest.java
@@ -19,6 +19,7 @@ package org.osc.core.broker.service.servicefunctionchain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -27,6 +28,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.osc.core.broker.model.entities.virtualization.ServiceFunctionChain;
 import org.osc.core.broker.service.dto.BaseDto;
 import org.osc.core.broker.service.request.AddOrUpdateServiceFunctionChainRequest;
 import org.osc.core.broker.service.response.BaseJobResponse;
@@ -35,9 +37,9 @@ import org.osc.core.broker.service.response.BaseJobResponse;
 public class UpdateServiceFunctionChainServiceTest extends BaseServiceFunctionChainServiceTest {
 	@InjectMocks
 	private UpdateServiceFunctionChainService service;
-	
+
 	AddOrUpdateServiceFunctionChainRequest request;
-	
+
 	@Override
 	@Before
 	public void testInitialize() throws Exception {
@@ -56,40 +58,59 @@ public class UpdateServiceFunctionChainServiceTest extends BaseServiceFunctionCh
 	}
 
 	@Test
-	public void testDispatch_WithNonEmptyListOfVsIdsUpdateValidationSuccessful_ValidationSucceeds() throws Exception {
-		
+	public void testDispatch_WhenVirtualSystemListEmpty_SfcIsUpdated() throws Exception {
+
+		// Arrange
 		BaseDto dto = new BaseDto();
 		dto.setId(this.sfc.getId());
 		dto.setParentId(this.vc.getId());
 		request.setDto(dto);
 		request.setName(this.sfc.getName());
 		List<Long> vsIds = new ArrayList<Long>();
-		vsIds.add(this.vs.getId());
-		vsIds.add(this.vs1.getId());
-		request.setVirtualSystemIds(vsIds);
+		
 		Mockito.when(this.service.validator.validateAndLoad(request)).thenReturn(this.sfc);
+
 		// Act.
 		BaseJobResponse response = this.service.dispatch(request);
+		List<Long> sfcVsIdList = this.em.find(ServiceFunctionChain.class, this.sfc.getId()).getVirtualSystems().stream()
+                .map(vss -> vss.getId()).collect(Collectors.toList());
+
+		// Assert
+		Assert.assertEquals("The list of virtual system ids is not Empty", vsIds, sfcVsIdList);
 		Assert.assertNotNull("The returned response should not be null.", response);
+		Assert.assertEquals("The return Id should be equal to sfc Id", response.getId(), this.sfc.getId());
 	}
-	
+
+
 	@Test
-	public void testDispatch_WhenVsIdsListUpdateValidationSuccessful_ValidationSucceeds() throws Exception {
-		
+	public void testDispatch_WhenVirtualSystemListNotEmpty_SfcIsUpdatedAndListInOrder() throws Exception {
+
+		// Arrange
 		BaseDto dto = new BaseDto();
 		dto.setId(this.sfc.getId());
 		dto.setParentId(this.vc.getId());
 		request.setDto(dto);
 		request.setName(this.sfc.getName());
 		List<Long> vsIds = new ArrayList<Long>();
+
 		vsIds.add(this.vs1.getId());
 		vsIds.add(this.vs.getId());
 		request.setVirtualSystemIds(vsIds);
 		Mockito.when(this.service.validator.validateAndLoad(request)).thenReturn(this.sfc);
+
 		// Act.
+		// update sfc with virtual system ids list {2,1}
 		BaseJobResponse response = this.service.dispatch(request);
+
+		// check Virtual system is as updated
+		List<Long> sfcVsIdList = this.em.find(ServiceFunctionChain.class, this.sfc.getId()).getVirtualSystems().stream()
+				.map(vss -> vss.getId()).collect(Collectors.toList());
+		// read back sfc and cross check with local Virtual system list for
+		// order
+
+		// Assert
+		Assert.assertEquals("The list of virtual system ids is different than expected", vsIds, sfcVsIdList);
 		Assert.assertNotNull("The returned response should not be null.", response);
+		Assert.assertEquals("The return Id should be equal to sfc Id", response.getId(), this.sfc.getId());
 	}
-	
-	//TODO : karimull  Add more test cases in looking for updated in order. Need some ramp up here
 }
