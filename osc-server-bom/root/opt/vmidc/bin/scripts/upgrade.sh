bce23805b85a9d4f034495b08f39be7bb7f2f1dd
dmitry <dmitry.mx.gerenrot@intel.com>
2017-11-21 15:17:27 -0800

    remove extraneous jks. Add osctrustore under server-bom

--- osc-server-bom/root/opt/vmidc/bin/scripts/upgrade.sh
+++ osc-server-bom/root/opt/vmidc/bin/scripts/upgrade.sh
@@ -48,7 +48,7 @@ upgrade_java() {
     jre_tgz=$tmp_root/jre-8u112-linux-x64.tar.gz
     jre_dir=$my_root/opt/vmidc/jre
     if [ -f "$jre_tgz" ]; then
-	tar -xz --directory=$jre_dir -f $jre_tgz --strip 1
+    tar -xz --directory=$jre_dir -f $jre_tgz --strip 1
     fi
 }
 
@@ -56,7 +56,7 @@ upgrade_keystore() {
     old_keystore=$my_root/opt/vmidc/bin/vmidcKeyStore.jks
     old_truststore=$my_root/opt/vmidc/bin/vmidctruststore.jks
     new_truststore=$my_root/opt/vmidc/bin/osctrustore.jks
-    keytool=/opt/vmidc/jre/bin/keytool
+    keytool=$my_root/opt/vmidc/jre/bin/keytool
 
     if [ ! -f "$new_truststore" ]; then
     cp $old_truststore $new_truststore
